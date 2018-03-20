e2463cbd47e72675ef6f824108868fe10bc70ecf
varunreddy12 <36818798+varunreddy12@users.noreply.github.com>
2018-03-20 22:39:36 +0530

    Update HomeController.java

--- src/main/java/org/mitre/web/HomeController.java
+++ src/main/java/org/mitre/web/HomeController.java
@@ -31,10 +31,6 @@ public class HomeController {
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
 		return "home";
 	}
 	
